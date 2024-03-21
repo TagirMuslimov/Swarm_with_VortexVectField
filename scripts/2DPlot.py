8c19d6a1d2fe726045224bccba6bfba62112df4c
TagirMuslimov <tagir.muslimov@gmail.com>
2024-03-21 17:16:11 +0500

    new file:   cache/0CC7FD0D.json 	new file:   cache/CDB45029.json 	new file:   data/20240320-183644_CurlFree4Copt.csv 	new file:   data/20240320-190554_CurlFree4Copt.csv 	new file:   data/20240320-192331_NoCurlFree4Copt.csv 	new file:   data/20240320-193007_NoAPF4Copt.csv 	new file:   data/20240321-154300_NoCurlFree4Copt2ndOrder.csv 	new file:   data/20240321-162838_CurlFree4Copt2ndOrder.csv 	new file:   data/20240321-163310_CurlFree4Copt2ndOrder.csv 	new file:   data/20240321-163926_NoAPF4Copt2ndOrder.csv 	new file:   scripts/2DPlot.py 	new file:   scripts/3DPlot.py 	new file:   src/CurlFree_4copters.py 	new file:   src/CurlFree_4copters_2ndOrder.py 	new file:   src/cache/0CC7FD0D.json 	new file:   src/cache/CDB45029.json

---
+++ scripts/2DPlot.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from matplotlib import cm
+
+
+plt.rcParams["figure.autolayout"] = True
+
+df = pd.read_csv(r'C:\Users\tagir\Documents\![Мои научные статьи]\Статья Информатика и АВтоматизация 23\![Data]\3rd try\sim3_20240319-225449_NoAPF.csv', header=0, delimiter=';')
+
+cols = df.columns
+for col in cols:
+    df[col] = df[col].astype(float)
+
+print(df.columns)
+
+my_cmap = cm.plasma
+
+# df.plot(x='i', y=['setPx1','setPx2','setPx3'])
+# df.plot(x='i', y=['setPy1','setPy2','setPy3'])
+# df.plot(x='i', y=['p_12', 'p_23'])
+# ax = df.plot(x='i', y=['distance_12', 'distance_13','distance_14','distance_23','distance_24', 'distance_34'], markevery=[-1], marker='o', markerfacecolor='r', markeredgecolor='r', title = 'Distances')
+ax = df.plot(x='i', y=['distance_12', 'distance_13','distance_14','distance_23','distance_24', 'distance_34'])
+ax.set(xlabel='Time, [ds]', ylabel='Distance between UAVs, [m]')
+
+# ax = df.plot(x='i', y=['distance_12'])
+# ax.set(xlabel='Time, [ds]', ylabel='Distance between UAVs, [rad]')
+
+# df.plot(x='i', y=['d_1','d_2', 'd_3'])
+fig, ax = plt.subplots()
+plt_1, = ax.plot(df['px_1'], df['py_1'], label='Copter #1', c=my_cmap(0.09), markevery=[-1], marker='o', markerfacecolor=my_cmap(0.09), markeredgecolor=my_cmap(0.09), markersize=9)
+plt_2, = ax.plot(df['px_2'], df['py_2'], label='Copter #2', c=my_cmap(0.38), markevery=[-1], marker='o', markerfacecolor=my_cmap(0.38), markeredgecolor=my_cmap(0.38), markersize=9)
+plt_3, = ax.plot(df['px_3'], df['py_3'], label='Copter #3', c=my_cmap(0.62), markevery=[-1], marker='o', markerfacecolor=my_cmap(0.62), markeredgecolor=my_cmap(0.62), markersize=9)
+plt_4, = ax.plot(df['px_4'], df['py_4'], label='Copter #4', c=my_cmap(0.8), markevery=[-1], marker='o', markerfacecolor=my_cmap(0.8), markeredgecolor=my_cmap(0.8), markersize=9)
+ax.legend()
+ax.set(xlabel='East-West Position, [m]', ylabel='North-South Position, [m]', title = 'Trajectories')
+# ax.legend([plt_1, plt_2], ['label1', 'label2'])
+
+
+
+# df.plot(x='px_1', y=['py_1'])
+# df.plot(x='px_2', y=['py_2'])
+# ax = df.plot(x='px_1', y=['py_1'])
+# df.plot(x='px_2', y=['py_2'], ax=ax)
+
+###############################################
+# fig, axes = plt.subplots(nrows=3, ncols=1)
+# ax=df.plot(x='px_1', y=['py_1'], ax=axes[0])
+# ax.set(xlabel='px_1, [m]', ylabel='py_1, [m]')
+
+# ax=df.plot(x='px_2', y=['py_2'], ax=axes[1])
+# ax.set(xlabel='px_2, [m]', ylabel='py_2, [m]')
+
+# ax=df.plot(x='px_3', y=['py_3'], ax=axes[2])
+# ax.set(xlabel='px_3, [m]', ylabel='py_3, [m]')
+
+# df.plot(x='setPx3', y=['setPy3'])
+plt.show()
---
+++ scripts/3DPlot.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+ax = plt.axes(projection='3d')
+
+# headers = ['i', 'T_Z', 'v_z', 'CX', 'CY', 'k', 'R', 'D_12', 'v_f', 'v_cruis', 'k_f', 'p12', 'px_1', 'py_1', 'pz_1', 'd_1', 'phi_1', 'angle_1', 'v1', 'vx1', 'vy1', 'setPx1', 'setPy1', 'px_2', 'py_2', 'pz_2', 'd_2', 'phi_2', 'angle_2', 'v2', 'vx2', 'vy2', 'setPx2', 'setPy2']
+
+# df = pd.read_csv('/home/tagir/CircularMotion/20221014-140915_setpos2.csv', 
+#             names=headers, header=0, delimiter=';',
+#             skip_blank_lines=True,  engine='python'         
+#             )
+
+# cols = df.columns
+# for col in cols:
+#     df[col] = df[col].astype(float)
+# df.set_index('i').plot()
+
+
+# df = pd.read_csv(r'C:\Users\tagir\Documents\![Crazyflie Flight Tests]\![From Github]\CircularMotion\data\most valuable\3 copters with a moving center\sim3_20240302-203117_NoCurl.csv', header=0, delimiter=';')
+df = pd.read_csv(r'C:\Users\tagir\Documents\![Мои научные статьи]\Статья Информатика и АВтоматизация 23\![Data]\3rd try\sim3_20240319-224528_Curl.csv', header=0, delimiter=';')
+cols = df.columns
+for col in cols:
+    df[col] = df[col].astype(float)
+
+print(df.columns)
+
+my_cmap = cm.plasma
+
+x_1 = df['px_1']
+y_1 = df['py_1']
+z_1 = df['pz_1']
+x_2 = df['px_2']
+y_2 = df['py_2']
+z_2 = df['pz_2']
+x_3 = df['px_3']
+y_3 = df['py_3']
+z_3 = df['pz_3']
+x_4 = df['px_4']
+y_4 = df['py_4']
+z_4 = df['pz_4']
+
+plt.cla()
+
+# fig, ax = plt.subplots()
+
+plt_1 = ax.plot3D(x_1, y_1, z_1, label='Copter #1', c=my_cmap(0.09), markevery=[-1], marker='o', markerfacecolor=my_cmap(0.09), markeredgecolor=my_cmap(0.09), markersize=9, linewidth=2)
+plt_2 = ax.plot3D(x_2, y_2, z_2, label='Copter #2', c=my_cmap(0.38), markevery=[-1], marker='o', markerfacecolor=my_cmap(0.38), markeredgecolor=my_cmap(0.38), markersize=9, linewidth=2)
+plt_3 = ax.plot3D(x_3, y_3, z_3, label='Copter #3', c=my_cmap(0.62), markevery=[-1], marker='o', markerfacecolor=my_cmap(0.62), markeredgecolor=my_cmap(0.62), markersize=9, linewidth=2)
+plt_4 = ax.plot3D(x_4, y_4, z_4, label='Copter #4', c=my_cmap(0.8), markevery=[-1], marker='o', markerfacecolor=my_cmap(0.8), markeredgecolor=my_cmap(0.8), markersize=9, linewidth=2)
+
+ax.set_xlabel('East-West Position, [m]')
+ax.set_ylabel('North-South Position, [m]')
+ax.set_zlabel('Altitude, [m]')
+
+
+plt.show()
+
+# plt.savefig('position.eps')
